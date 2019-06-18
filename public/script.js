bce3d885c9048753b871407810a6ed88bea7ba2a
Nimish Santosh <nimishsantosh107@icloud.com>
2019-06-18 23:55:55 +0530

    updated easy site

--- public/script.js
+++
@@ -1,6 +0,0 @@
-//var iframe = document.querySelectorAll("iframe")[1];
-var trackID = document.querySelector("#trackID");
-var copyButton = document.querySelector("#copyButton").addEventListener('click',function () {
-	trackID.select();
-	document.execCommand("copy");
-});
